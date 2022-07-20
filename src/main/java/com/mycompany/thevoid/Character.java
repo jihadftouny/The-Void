f06061a31edabb1636eff9b829e0e5a4023a479a
Jihanger <jihad.santos@hotmail.com>
2022-07-20 09:28:24 +0300

    added some variables
    started implementing stat mods, AC, and Equipment

--- src/main/java/com/mycompany/thevoid/Character.java
+++ src/main/java/com/mycompany/thevoid/Character.java
@@ -9,23 +9,38 @@ package com.mycompany.thevoid;
  * @author jihad
  */
 public abstract class Character {
+
+    
+    
+    //BIG
+    //BIG BIG BIG IMPORTANT NOTE:::::::::::::::::::::::::::::::::::
+    // might have to create methods like attack() and defend() (overidable) to set player stats everytime an item is equipped
+    // will have to create setArmor, setWeapon, etc. both for player and enemies, while also randomly picking one from a range to enemies
     
     //variables
     String name;
     int hp, maxHp, xp;
-    int[] Stats; //every stat level up on the player (every upgrade) is a 3d4
+    int[] Stats; //every level up player has x points to spend on his attributes
+    int ArmorClass;
+    
+    //modifiers
+    int strM, dexM, conM, intM, wisM, chaM;
+
+    String[] EquipmentOn;
 
     public Character(String name, int maxHp, int xp) {
-        this.Stats = new int[]{10,10,10,10,10,10}; //STR,DEX,CON,INT,WIS,CHA, -future update - CON(hp), AGI (chance to dodge), INT (when we add spells) - - also make every upgrade a bigger difference
+        this.Stats = new int[]{10, 10, 10, 10, 10, 10}; //STR,DEX,CON,INT,WIS,CHA, -future update - CON(hp), AGI (chance to dodge), INT (when we add spells) - - also make every upgrade a bigger difference
+        this.ArmorClass = 10 + dexM;
+        this.EquipmentOn = new String[]{"", "", ""};
         this.name = name;
         this.maxHp = maxHp;
         this.xp = xp;
         hp = maxHp;
     }
-    
-    
+
     //methods every character has
     public abstract int attack();
+
     public abstract int defend();
-    
+
 }
