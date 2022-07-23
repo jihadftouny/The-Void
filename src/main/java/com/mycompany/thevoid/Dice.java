d19464f83ee69623b28499d988adb0267bf0e91b
Jihanger <jihad.santos@hotmail.com>
2022-07-23 12:27:32 +0300

    added different sets of dice

--- src/main/java/com/mycompany/thevoid/Dice.java
+++ src/main/java/com/mycompany/thevoid/Dice.java
@@ -27,7 +27,7 @@ public class Dice {
         //NEW CHARACTER DIE
     public static Dice startStatsDice = new Dice(4,6);
     
-    // SINGLE DIE
+    // DIE SETS
         //SINGLE DIE
     public static Dice d2 = new Dice(1, 2);
     public static Dice d4 = new Dice(1, 4);
@@ -36,6 +36,24 @@ public class Dice {
     public static Dice d10 = new Dice(1, 10);
     public static Dice d12 = new Dice(1, 12);
     public static Dice d20 = new Dice(1, 20);
+    
+        //DOUBLE DIE
+    public static Dice twod2 = new Dice(2, 2);
+    public static Dice twod4 = new Dice(2, 4);
+    public static Dice twod6 = new Dice(2, 6);
+    public static Dice twod8 = new Dice(2, 8);
+    public static Dice twod10 = new Dice(2, 10);
+    public static Dice twod12 = new Dice(2, 12);
+    public static Dice twod20 = new Dice(2, 20);
+    
+       //TRIPLE DIE
+    public static Dice threed2 = new Dice(3, 2);
+    public static Dice threed4 = new Dice(3, 4);
+    public static Dice threed6 = new Dice(3, 6);
+    public static Dice threed8 = new Dice(3, 8);
+    public static Dice threed10 = new Dice(3, 10);
+    public static Dice threed12 = new Dice(3, 12);
+    public static Dice threed20 = new Dice(3, 20);
     
     
     public Dice(int quantity, int sides) {
