a91a77ce8cd5cdd71e3c6ce68114d88c7f471739
Jihanger-997 <jihad.santos@hotmail.com>
2022-08-22 12:25:32 +0300

    implemented CR system
    function on line 26 uses 2 variables a and b and playerLevel (which is the act the player is in). An if else chain sets the playerLevel variable to a random value based on DnD levels of play.
    playerLevel is then used in Challenge Rating formula

--- src/main/java/com/mycompany/thevoid/Enemy.java
+++ src/main/java/com/mycompany/thevoid/Enemy.java
@@ -18,9 +18,48 @@ public class Enemy extends Character {
 
     public Enemy(String name, int playerXp) {
         super(name, 1, (int) (Math.random() * (playerXp / 4 + 2) + 1)); //name maxhp xp
-        this.playerXp = playerXp;
+        this.playerXp = playerXp; //this here sets the variable playerXp with the variable that was given in the parameter declaration (which was set on object creation)
         Stats[0] = 10 + (int) (Math.random() * (playerXp / 4 + 1) + xp / 4 + 3);
         Stats[1] = 10 + (int) (Math.random() * (playerXp / 4 + 1) + xp / 4 + 3);
+    }
+
+    public void setStatsEnemy() {
+
+        double a;
+        double b;
+        int playerLevel = GameLogic.act;
+
+        //this if else chain will set playerLevel randomyl based on act, utilizing DnD Levels of play
+        if (playerLevel == 4) {
+            playerLevel = (int) (Math.random() * (21 - 17)) + 17;   // 20-17 (max-min)
+        } else if (playerLevel == 3) {
+            playerLevel = (int) (Math.random() * (17 - 11)) + 11;   // 16-11
+        } else if (playerLevel == 2) {
+            playerLevel = (int) (Math.random() * (11 - 5)) + 5;     // 10-5
+        } else {
+            playerLevel = (int) (Math.random() * (5 - 1)) + 1;      // 5-1
+        }
+
+        double CRmax, CRmin, CRboss;
+
+        //The following will set max and minimum Challenge Ratings based on playerLevel
+        //min
+        a = 0.087970550572;
+        b = 1.4100213799828;
+        CRmin = a * Math.pow(playerLevel, b);
+
+        //max
+        a = 0.2320721484759;
+        b = 1.319592522878;
+        CRmax = a * Math.pow(playerLevel, b);
+
+        //boss
+        a = 0.4294334810279;
+        b = 1.165240310288;
+        CRboss = a * Math.pow(playerLevel, b);
+
+        System.out.println("min max boss\n" + CRmin + " " + CRmax + " " + CRboss);
+
     }
 
     @Override
