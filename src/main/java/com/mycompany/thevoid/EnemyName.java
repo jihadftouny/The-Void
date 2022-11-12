f9691ef7c87be47f626c91b4cd178c696939a9be
Jihanger-997 <jihad.santos@hotmail.com>
2022-11-12 18:58:12 +0200

    Update to Enemy Names designs

--- src/main/java/com/mycompany/thevoid/EnemyName.java
+++ src/main/java/com/mycompany/thevoid/EnemyName.java
@@ -33,11 +33,10 @@ public class EnemyName {
         ArrayList<Integer> weightLast = new ArrayList<Integer>();
 
         // each name will provide different bonuses and skills + the base stats and skills of the lastName
-        //<editor-fold defaultstate="collapsed" desc="Chapter 1 - Undercity">
-        if (GameLogic.act == 1) { //UNDERCITY
-            //<editor-fold defaultstate="collapsed" desc="Beasts">
+        if (GameLogic.act == 1) {       //<editor-fold defaultstate="collapsed" desc="Chapter 1 - Undercity DONE">
+            //<editor-fold defaultstate="collapsed" desc="1 - Beasts">
             if ("Beast".equals(type)) {
-                // FIRST NAME - STATS mods
+                // FIRST NAME - STATS mods 
                 firstName.add("Feral");// add STR DEX
                 weightFirst.add(20);
                 firstName.add("Aerobicized");// add CON
@@ -84,7 +83,8 @@ public class EnemyName {
                 weightLast.add(20);
 
             } //</editor-fold>
-            //<editor-fold defaultstate="collapsed" desc="Humanoids">
+            
+            //<editor-fold defaultstate="collapsed" desc="1 - Humanoids">
             else if ("Humanoid".equals(type)) {
                 //STATS mods
                 //Tier 1
@@ -128,10 +128,92 @@ public class EnemyName {
                 lastName.add("Conjurer");
                 weightLast.add(20);
             } //</editor-fold>
+            
+            //<editor-fold defaultstate="collapsed" desc="1 - Mechs">
+            else if ("Mech".equals(type)) {
+                //STATS mods
+                //Tier 1
+                firstName.add("Steamed");// add STR DEX
+                weightFirst.add(20);
+                firstName.add("Reinforced");// add CON
+                weightFirst.add(20);
+                firstName.add("Plated");// add AC
+                weightFirst.add(20);
+
+                // Tier 2
+                firstName.add("Modified"); // add random 2 stats
+                weightFirst.add(10);
+                firstName.add("Rusty"); // remove random 2 stats
+                weightFirst.add(10);
+
+                // Tier 3 -- will modify the whole design of the humanoid (post-release)
+                firstName.add("Hexa"); // add STR DEX CON + AC
+                weightFirst.add(5);
+                firstName.add("Ripper"); // add STR DEX CON AC A LOT
+                weightFirst.add(3);
+                firstName.add("K1-LA"); // super op 
+                weightFirst.add(2);
+
+                // MIDDLE NAMES - ELEMENTS
+                middleName.add("Pyro-Core"); 
+                weightMiddle.add(1);
+                middleName.add("Cryo-Core"); 
+                weightMiddle.add(1);
+                middleName.add("Electro-Core"); 
+                weightMiddle.add(1);
+
+                // LAST NAMES (Actual creature's name)
+                lastName.add("Battlebot");
+                weightLast.add(1);
+                lastName.add("Auto-Turret");
+                weightLast.add(1);
+                lastName.add("Drone");
+                weightLast.add(1);
+            } //</editor-fold>
+            
+            //<editor-fold defaultstate="collapsed" desc="1 - Magicals">
+            else if ("Magical".equals(type)) {
+                //STATS mods
+                //Tier 1
+                firstName.add("Glowing");// add STR DEX
+                weightFirst.add(20);
+                firstName.add("Chromated");// add CON
+                weightFirst.add(20);
+                firstName.add("Gilded");// add AC
+                weightFirst.add(20);
+
+                // Tier 2
+                firstName.add("Transmuted"); // add random 2 stats
+                weightFirst.add(10);
+                firstName.add("Fading"); // remove random 2 stats
+                weightFirst.add(10);
+
+                // Tier 3 - killing one of these can add or remove karma a lot
+                firstName.add("Corrupted"); // add STR DEX CON + AC
+                weightFirst.add(5);
+                firstName.add("Blessed"); // add STR DEX CON AC A LOT
+                weightFirst.add(5);
+
+
+                // MIDDLE NAMES - ELEMENTS
+                middleName.add("Pyro"); 
+                weightMiddle.add(1);
+                middleName.add("Cryo"); 
+                weightMiddle.add(1);
+                middleName.add("Electro"); 
+                weightMiddle.add(1);
+
+                // LAST NAMES (Actual creature's name)
+                lastName.add("Pixie");
+                weightLast.add(1);
+                lastName.add("Wisp");
+                weightLast.add(1);
+                lastName.add("Gazer");
+                weightLast.add(1);
+            } //</editor-fold>
         } //</editor-fold>
-        //<editor-fold defaultstate="collapsed" desc="Chapter 2 - Upside Down">
-        else if (GameLogic.act == 2) { //UPSIDE DOWN
-            //<editor-fold defaultstate="collapsed" desc="Beasts">
+        else if (GameLogic.act == 2) {  // <editor-fold defaultstate="collapsed" desc="Chapter 2 - Upside Down DONE">
+            //<editor-fold defaultstate="collapsed" desc="2 - Beasts">
             if ("Beast".equals(type)) {
                 //STATS mods
 
@@ -177,7 +259,8 @@ public class EnemyName {
                 weightLast.add(20);
 
             } //</editor-fold>
-            //<editor-fold defaultstate="collapsed" desc="Humanoids">
+            
+            //<editor-fold defaultstate="collapsed" desc="2 - Humanoids">
             else if ("Humanoid".equals(type)) {
                 //STATS mods
                 //Tier 1
@@ -187,7 +270,7 @@ public class EnemyName {
                 weightFirst.add(30);
 
                 // Tier 2
-                firstName.add("Raving");// Extra STR and DEX
+                firstName.add("Enraged");// Extra STR and DEX
                 weightFirst.add(15);
                 firstName.add("Ruined");// Less all stats
                 weightFirst.add(15);
@@ -197,37 +280,64 @@ public class EnemyName {
                 weightFirst.add(5);
                 firstName.add("Maddened"); // add STR DEX CON AC A LOT
                 weightFirst.add(3);
-                firstName.add("Demented"); // add STR DEX CON AC A LOT + immunity to some elements
+                firstName.add("Faceless"); // add STR DEX CON AC A LOT + immunity to some elements
                 weightFirst.add(2);
 
                 // MIDDLE NAMES
                 middleName.add(""); // None, humans won't have elemental names
                 weightMiddle.add(1);
 
+
                 // LAST NAMES (Actual creature's name)
-                lastName.add("Thug");
+                lastName.add("Thug Aspect");
                 weightLast.add(20);
-                lastName.add("Psycho");
+                lastName.add("Psycho Aspect");
                 weightLast.add(20);
-                lastName.add("Punk");
+                lastName.add("Punk Aspect");
                 weightLast.add(20);
-                lastName.add("Stalker"); //STALKER
+                lastName.add("Stalker Aspect"); //STALKER
                 weightLast.add(20);
-                lastName.add("Conjurer");
+                lastName.add("Conjurer Aspect");
                 weightLast.add(20);
+            } //</editor-fold>
+            
+            //<editor-fold defaultstate="collapsed" desc="2 - Magicals">
+            else if ("Magical".equals(type)) {
+                //STATS mods
+                //Tier 1
+
+                // Tier 2
+
+                // Tier 3 -- will modify the whole design of the humanoid (post-release)
+                firstName.add("Elder"); // add STR DEX CON + AC
+                weightFirst.add(60);
+                firstName.add("Eldritch"); // add STR DEX CON AC A LOT
+                weightFirst.add(30);
+                firstName.add("Deathwhispering"); // super op 
+                weightFirst.add(10);
+
+                // MIDDLE NAMES - ELEMENTS
+                middleName.add(""); 
+                weightMiddle.add(1);
+
+                // LAST NAMES (Actual creature's name)
+                lastName.add("Beholder");
+                weightLast.add(1);
+                lastName.add("Djinn");
+                weightLast.add(1);
+                lastName.add("Horror");
+                weightLast.add(1);
             } //</editor-fold>
 
         } //</editor-fold>
-        //<editor-fold defaultstate="collapsed" desc="Chapter 3 - Ashen Eternal City">
-        else if (GameLogic.act == 3) { //White City - Here we will have only 'nightmares' https://www.artstation.com/midnight-98
-            //<editor-fold defaultstate="collapsed" desc="Nightmare">
+        else if (GameLogic.act == 3) {  //White City - Here we will have only 'nightmares' https://www.artstation.com/midnight-98 //<editor-fold defaultstate="collapsed" desc="Chapter 3 - Ashen Eternal City">
+            //<editor-fold defaultstate="collapsed" desc="3 - Nightmares">
             if ("Nightmare".equals(type)) {
 
             } //</editor-fold>
         } //</editor-fold>
-        //<editor-fold defaultstate="collapsed" desc="Chapter 4 - The Inner World">
-        else if (GameLogic.act == 4) { //The Inner World
-            //<editor-fold defaultstate="collapsed" desc="Beasts">
+        else if (GameLogic.act == 4) {  //<editor-fold defaultstate="collapsed" desc="Chapter 4 - The Inner World">
+            //<editor-fold defaultstate="collapsed" desc="4 - Beasts">
             if ("Beast".equals(type)) {
                 //STATS mods
                 firstName.add("Furious");// add STR DEX
@@ -267,7 +377,72 @@ public class EnemyName {
                 lastName.add("Chimaera"); //Goat+Snake+Lion+Lizard
                 weightLast.add(20);
             } //</editor-fold>
+            
+            //<editor-fold defaultstate="collapsed" desc="4 - Humanoids">
+            else if ("Humanoid".equals(type)) {
+                //STATS mods
+                //Tier 1
+                firstName.add("Gilded");// More AC
+                weightFirst.add(50);
+                firstName.add("Chromated");// More+ AC
+                weightFirst.add(50);
+                
 
+                // MIDDLE NAMES (maybe force/psychic elements war)
+                middleName.add("Bestial");
+                weightMiddle.add(26);
+                middleName.add("Hallowed");
+                weightMiddle.add(25);
+                middleName.add("Corrupt");
+                weightMiddle.add(12);
+                middleName.add("Celestial");
+                weightMiddle.add(25);
+                middleName.add("Hellish");
+                weightMiddle.add(12);
+
+                // LAST NAMES (Actual creature's name) 
+                lastName.add("Centaur");
+                weightLast.add(12);
+                lastName.add("Ipotane");
+                weightLast.add(12);
+                lastName.add("Harpy");
+                weightLast.add(12);
+                lastName.add("Naga");
+                weightLast.add(12);
+                lastName.add("Satyr");
+                weightLast.add(12);
+                lastName.add("Sphinx");
+                weightLast.add(12);
+                lastName.add("Minotaur");
+                weightLast.add(12);
+                lastName.add("Gorgon");
+                weightLast.add(12);
+            } //</editor-fold>
+            
+            //<editor-fold defaultstate="collapsed" desc="4 - Ancestrals">
+            else if ("Ancestral".equals(type)) {
+                // ANCESTRALS ARE UNIQUE. ONLY THEIR OWN NAME
+                //STATS mods
+                firstName.add(""); 
+                weightFirst.add(1);
+
+                // MIDDLE NAMES - ELEMENTS
+                middleName.add(""); 
+                weightMiddle.add(1);
+
+                // LAST NAMES (Actual creature's name)  need further design (names)
+                lastName.add("The Knight"); // horse chess piece from pinterest
+                weightLast.add(1);
+                lastName.add("The Counselor"); //council of archmage pinterest
+                weightLast.add(1);
+                lastName.add("Gaea"); //like gaia but has technology mixed in it
+                weightLast.add(1);
+                lastName.add("Sif"); //like Sif in dark souls, maybe a different animal
+                weightLast.add(1);
+                lastName.add("Death"); // An undead dragon, but walks like a human
+                weightLast.add(1);
+            } //</editor-fold>
+            
         } // </editor-fold>
 
         // this function will set each names of the monsters in order (if they should (based on chance))
@@ -288,13 +463,11 @@ public class EnemyName {
 
     public String selectName(ArrayList<String> names, ArrayList<Integer> weight, boolean isLastName) { //firstName firstWeight
 
-
         //this list will be filled with n weight amounts of each name in names
         ArrayList<String> fullList = new ArrayList<>();
 
         Random rand = new Random();
 
-        
         for (int i = 0; i < names.size(); i++) {
 
             boolean isName; //will be used as a percentage that must be achieved to generate the name
@@ -314,9 +487,9 @@ public class EnemyName {
 
             }
         }
-        
+
         //will genearate a number equal to ArrayList size
-        int randomArraySelector = rand.nextInt(fullList.size()); 
+        int randomArraySelector = rand.nextInt(fullList.size());
 
         String returnString;
 
